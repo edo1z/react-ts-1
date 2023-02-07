c1531c54f3d006e192b90231d72ea2eec6f2e208
web3ten0 <web3ten0@gmail.com>
2023-02-07 10:17:48 +0900

    icon button

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,23 @@
-import { Box } from '@chakra-ui/react'
+import { Box, IconButton } from "@chakra-ui/react";
+import { BiLogIn } from "react-icons/bi";
 
 function NavBar() {
   return (
-    <Box w='100%' p={4} color='white' bgGradient="linear(to-t, green.200, blue.500)">
+    <Box
+      w="100%"
+      p={4}
+      color="white"
+      bgGradient="linear(to-t, green.500, blue.500)"
+    >
       NavBar
+      <IconButton
+        aria-label="Search database"
+        colorScheme="blue"
+        size="lg"
+        variant="outline"
+        icon={<BiLogIn />}
+      />
     </Box>
-  )
+  );
 }
-export default NavBar
+export default NavBar;
