7633f0f22a23a769990d7b172d9243a1b842493f
web3ten0 <web3ten0@gmail.com>
2023-02-07 10:39:20 +0900

    NavBarデザイン修正

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,23 +1,41 @@
-import { Box, IconButton } from "@chakra-ui/react";
+import {
+  Box,
+  IconButton,
+  Flex,
+  Spacer,
+  Heading,
+  ButtonGroup,
+  Button,
+} from "@chakra-ui/react";
 import { BiLogIn } from "react-icons/bi";
 
 function NavBar() {
   return (
-    <Box
-      w="100%"
-      p={4}
-      color="white"
-      bgGradient="linear(to-t, green.500, blue.500)"
-    >
-      NavBar
-      <IconButton
-        aria-label="Search database"
-        colorScheme="blue"
-        size="lg"
-        variant="outline"
-        icon={<BiLogIn />}
-      />
-    </Box>
+    <>
+      <Flex
+        minWidth="max-content"
+        alignItems="center"
+        gap="2"
+        p={4}
+        bg="green.300"
+      >
+        <Box p="2">
+          <Heading size="md">NavBar</Heading>
+        </Box>
+        <Spacer />
+        <ButtonGroup gap="2">
+          <Button colorScheme="teal">Sign Up</Button>
+          <Button colorScheme="teal">Log in</Button>
+          <IconButton
+            aria-label="Search database"
+            colorScheme="white"
+            size="lg"
+            variant="ghost"
+            icon={<BiLogIn />}
+          />
+        </ButtonGroup>
+      </Flex>
+    </>
   );
 }
 export default NavBar;
